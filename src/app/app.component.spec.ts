1561a0f3e724ba939f87bd843d441b229f9b7c8f
abchugh <achugh89@gmail.com>
2021-06-30 19:22:57 +0530

    CI workflow

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,15 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'structured-data-extractor'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('structured-data-extractor app is running!');
+    expect(compiled.querySelector('.content div span').textContent).toContain('Structured Data Extractor');
   });
 });
