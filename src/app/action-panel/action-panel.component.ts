2b2f7c05182b119ca2f0ddad0d868df558f9fa39
abchugh <achugh89@gmail.com>
2021-07-04 02:24:40 +0530

    Action and resourceList panel (partial)

--- src/app/action-panel/action-panel.component.ts
+++ src/app/action-panel/action-panel.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { ResourcePointer } from 'sophize-datamodel';
+import { save, State } from '../edit-panel/edit-panel.component';
+import { WorkspaceService } from '../workspace.service';
 
 @Component({
   selector: 'app-action-panel',
   templateUrl: './action-panel.component.html',
-  styleUrls: ['./action-panel.component.scss']
+  styleUrls: ['./action-panel.component.scss'],
 })
-export class ActionPanelComponent implements OnInit {
+export class ActionPanelComponent {
+  State = State;
+  state = State.IDLE;
+  status = '';
 
-  constructor() { }
+  constructor(private workspace: WorkspaceService) {}
 
-  ngOnInit(): void {
+  async load() {
+    const val = prompt('Enter resource pointer (example wiki/T_cone): ');
+    if (val) {
+      const ptr = ResourcePointer.fromString(val);
+      if (!ptr) {
+        alert('Invalid Input');
+        return;
+      }
+      try {
+        this.state = State.LOAD;
+        await lastValueFrom(this.workspace.load(ptr));
+      } catch (e) {
+        alert('Failed to load resource: ' + e);
+      } finally {
+        this.state = State.IDLE;
+      }
+    }
   }
 
+  addNew() {}
+
+  async saveAll() {
+    let numSaved = 0;
+    const keys = [...this.workspace.getLoadedResources().keys()];
+    for (const ptrStr of keys) {
+      try {
+        await save(
+          ResourcePointer.fromString(ptrStr),
+          this.workspace,
+          (state) => (this.state = state)
+        );
+        numSaved++;
+      } catch (e) {
+        this.status = `Error saving ${ptrStr}: ${e}`;
+        return;
+      }
+    }
+    this.status = `Saved ${numSaved} resources.`;
+  }
+
+  unload() {
+    for (const ptrStr of this.workspace.getLoadedResources().keys()) {
+      this.workspace.unload(ResourcePointer.fromString(ptrStr), false);
+    }
+  }
 }
--- src/app/resource-list-panel/resource-list-panel.component.ts
+++ src/app/resource-list-panel/resource-list-panel.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSelectionListChange } from '@angular/material/list';
+import { debounceTime } from 'rxjs/operators';
+import { ResourcePointer } from 'sophize-datamodel';
+import { WorkspaceService } from '../workspace.service';
 
 @Component({
   selector: 'app-resource-list-panel',
   templateUrl: './resource-list-panel.component.html',
-  styleUrls: ['./resource-list-panel.component.scss']
+  styleUrls: ['./resource-list-panel.component.scss'],
 })
 export class ResourceListPanelComponent implements OnInit {
-
-  constructor() { }
+  resourcePtrList = [] as string[];
+  constructor(private workspaceService: WorkspaceService) {}
 
   ngOnInit(): void {
+    this.updateList();
+    this.workspaceService.loadedResourceSubject$
+      .pipe(debounceTime(500))
+      .subscribe((_) => this.updateList());
   }
 
+  updateList() {
+    this.resourcePtrList = [
+      ...this.workspaceService.getLoadedResources().keys(),
+    ];
+  }
+
+  onSelectionChange(event: MatSelectionListChange) {
+    const ptr = ResourcePointer.fromString(
+      event.source?.selectedOptions?.selected[0]?.value
+    );
+    this.workspaceService.selectedPtr$.next(ptr);
+  }
 }
